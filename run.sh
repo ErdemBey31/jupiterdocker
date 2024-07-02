1de45e605fc0f5b39c3f95bdc9b363bda1975f00
FrgtMe <142883038+FrgtMe@users.noreply.github.com>
2024-07-02 13:28:45 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-cat pip.txt > ~/.config/pip/pip.conf
-pip3 install selenium notebook --break-system-packages
+npm i botgram -y
+pip install selenium notebook
 echo -e "e\ne" | jupyter notebook password 
 jupyter notebook --ip=0.0.0.0 --port=443 --allow-root
